dc3acf44d3ca25c4331d9c082b9f05210d66e256
John Ballesteros <johnshift.dev@gmail.com>
2021-10-07 09:49:16 +0800

    update eslint, try using env vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-const App = () => {
-  return <h1 data-testid="title">Hello DEV!</h1>;
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1 data-testid="title">Hello DEV!</h1>
+      <p>VITE_CUSTOM: {import.meta.env.VITE_CUSTOM}</p>
+    </div>
+  );
 };
 export default App;
