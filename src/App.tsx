8443979ff719a01bdb7d7e6130bacd388e448a41
John Ballesteros <johnshift.dev@gmail.com>
2021-10-07 22:41:26 +0800

    removed cypress

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+// components
+import Navbar from './components/navbar/Navbar';
+
+// pages
+import Home from './pages/Home';
+import Link1 from './pages/Link1';
+import Link2 from './pages/Link2';
+import Link3 from './pages/Link3';
+import NotFound from './pages/NotFound';
 
 const App = (): JSX.Element => (
   <div className="App">
-    <h1 id="hello-msg">
-      Hello Message:
-      {' '}
-      {process.env.REACT_APP_HELLO}
-    </h1>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/link-1">
+          <Link1 />
+        </Route>
+        <Route path="/link-2">
+          <Link2 />
+        </Route>
+        <Route path="/link-3">
+          <Link3 />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   </div>
 );
 
