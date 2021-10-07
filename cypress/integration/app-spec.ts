c058ca38ba4b1d4d95f522f9a4967a0cd5d91bf7
John Ballesteros <johnshift.dev@gmail.com>
2021-10-07 14:57:31 +0800

    fix integration test errors

--- cypress/integration/app-spec.ts
+++ cypress/integration/app-spec.ts
@@ -1,12 +1,12 @@
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
 
-describe("App", function() {
-  beforeEach(function() {
-    cy.visit("/");
+describe('App', () => {
+  beforeEach(() => {
+    cy.visit('/');
   });
 
-  it("renders the app", function() {
-    cy.get(".App-link").should("contain", "Learn React");
+  it('renders the app', () => {
+    cy.get('#hello-msg').should('contain', 'Hello Message:');
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = (): JSX.Element => (
   <div className="App">
-    <h1>
+    <h1 id="hello-msg">
       Hello Message:
       {' '}
       {process.env.REACT_APP_HELLO}
